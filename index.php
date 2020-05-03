bd9fa4b1b9dfe54b863ade662e672e1bcbbfd3d4
artytrik <tema-luch@mail.ru>
2020-05-03 12:17:21 +0300

    Добавляет функцию форматирования цены

--- index.php
+++ index.php
@@ -43,6 +43,14 @@ $goods = [
         'img' => 'img/lot-6.jpg'
     ]
 ];
+
+function format_price($old_price) {
+    $new_price = ceil($old_price);
+
+    $new_price = $new_price < 1000 ? $new_price . ' ₽' : number_format($new_price, 0, '', ' ') . ' ₽';
+
+    return $new_price;
+}
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -116,7 +124,7 @@ $goods = [
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$good['price']; ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=format_price($good['price']); ?></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
